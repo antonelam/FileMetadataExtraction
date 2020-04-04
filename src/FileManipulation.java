c01a0cc969cb0a6aca19b4cedb4b6a256dc9903d
antonela.mrkalj <antonela.mrkalj@CPX-H41XAL71V2G.dir.svc.accenture.com>
2020-04-04 17:15:08 +0100

    Code is now officially only fully functional from cmd. Please read comments in the code to see how to make it run again from Eclipse.

--- src/FileManipulation.java
+++ src/FileManipulation.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
@@ -28,22 +29,50 @@ public class FileManipulation {
 	String[] values = new String[3];
 	String[] allowed_fileTypes =  {".doc",".docx",".pdf"};
 	String[] special_fileTypes = {".xml", ".log"};
+	int count = 0; //count how many files generated
+	int count_correct = 0; //count how many files are correct format
+	int count_incorrect = 0; //count how many files are incorrect format
 	
 	public FileManipulation(String path) {
+		/*
+		 * This is a constructor
+		 * It creates file from path given and reads xml file
+		 * 
+		 */
 		try {
 			this.path = path;
-			//location of the xml template
-			this.doc = readXML("C:\\Users\\antonela.mrkalj\\git\\FileMetadataExtraction\\src\\generic.metadata.xml");
+			/* NAME of the xml template
+			 * NO NEED TO PUT WHOLE PATH ANYMORE
+			 * LEAVE HOW IT IS
+			 */
+			
+			this.doc = readXML("XML_Source\\generic.metadata.xml");
 			// path to the .doc, .docx, .pdf files
 			this.folder = new File(path);
+			
 		} catch (Exception e) {
+			System.out.println("Invalid path location.");
 			e.printStackTrace();
 		}
 	}
 
 	public void startMetadataExtraction() {
+		/*
+		 * This is start function
+		 * It will run only if invoked from Main
+		 * It will first check if file/folder exists (maybe path given is incorrect)
+		 * If correct, it calls recursive function listAllFiles - it goes through all files and folders within
+		 * If not correct, it will stop the program and inform the user
+		 */
 		try {
-			listAllFiles(this.folder);
+			if(this.folder.exists()) {
+				listAllFiles(this.folder);
+				System.out.println("Files generated:" + this.count);
+				System.out.println("Files passed verification:" + this.count_correct);
+				System.out.println("Files failed verification:" + this.count_incorrect);
+			}
+			else
+				System.out.println("The folder provided does not exist.");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -88,17 +117,35 @@ public class FileManipulation {
 			transformer = transformerFactory.newTransformer();
 			DOMSource source = new DOMSource(doc);
 			StreamResult result = new StreamResult(new File(path + "\\" + filename + ".metadata.properties.xml"));
+			//System.out.println("Generating:" + filename + ".metadata.properties.xml");
 			transformer.transform(source, result);
+			//System.out.println("Done...");
+			this.count ++; //little number counter that says how many files are generated
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("Done");
+		//System.out.println("Done...");
+		//this.count ++; //little number counter that says how many files are generated
 	}
 
 	public Document readXML(String path) throws ParserConfigurationException, SAXException, IOException {
 		// reading xml file - generic script
 		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		/*this WAS the code i used -- it tries to detect xml template location
+		 * It didn't work properly when xml was part of jar
+		 * so is useless now
+		 */
+		/*ClassLoader classLoader = getClass().getClassLoader();
+		URL resource = classLoader.getResource(path);
+        if (resource == null) {
+            throw new IllegalArgumentException("File is not found!");
+        } else {
+            return new File();
+        	System.out.println("Location of the xml template: " + resource.getPath());
+            Document doc = docBuilder.parse(resource.getFile());
+            return doc;
+        }*/
 		// path to generic xml file
 		Document doc = docBuilder.parse(path);
 		return doc;
@@ -120,12 +167,15 @@ public class FileManipulation {
 				listAllFiles(file_);
 			} else {
 				File temp = new File(file_.getPath() + ".metadata.properties.xml");
+				this.count_correct++;
 				if (!temp.exists()) { 
 					//code won't verify file name if corresponding .xml is generated -- stops overwritting
 					if (VerifyFileName(file_.getName())) {
 						// if name is correctly formated, generate .xml
 						updateXML(this.doc, folder.getPath(), file_.getName());
-					} else {				
+					} else {
+						this.count_correct--;
+						this.count_incorrect++;
 						rejected.add(folder.getName() +"\\"+ file_.getName());
 					}
 				}
@@ -226,7 +276,7 @@ public class FileManipulation {
 			}
 			else if(dir.isDirectory())//if file is folder allow it		
 				return true;
-			
+			this.count_incorrect++;
 			wrongFileTypes.add(dir.getName() +"\\"+ name);//if nothing from above, filter it out and add to list
 			return false;
 		};
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			//location of the main folder
-			String main_path = "C:\\Users\\antonela.mrkalj\\git\\FileMetadataExtraction\\TEST";
-			FileManipulation filegenerate = new FileManipulation(main_path);
-			filegenerate.startMetadataExtraction();
+			/*
+			 * The main function always expects ONE argument
+			 * String [] args is empty if nothing is given
+			 * When running program from eclipse, you are not giving arguments, therefore program will stop
+			 * You must run from command line to test functionality
+			 * Or comment out if statement and replace args[0] with your file path
+			 */
+			if(args.length == 1) {			
+				System.out.println(args[0]);
+				String main_path = args[0];//"C:\\Users\\antonela.mrkalj\\git\\FileMetadataExtraction\\TEST";
+				FileManipulation filegenerate = new FileManipulation(main_path);
+				filegenerate.startMetadataExtraction();
+			}
+			else
+				System.out.println("Expected input: one \nType: full path to the target folder.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
